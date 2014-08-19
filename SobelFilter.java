984564be1140372c39ff0c987b3ccc92133e8320
teslastears <google.teslastears@gmail.com>
2014-08-19 11:39:18 -0400

    Functional sobel operator
    Sobel operator functional but the code is messy. Need to clean up.

--- SobelFilter.java
+++ SobelFilter.java
@@ -10,21 +10,29 @@ public class SobelFilter{
 	static BufferedImage inputFile, outputFile;
 	static int width, height;
 	static File example, result;	
-	static double sobelKernel[][] = {{-1, 0, 1},		// Not used. Approximation example 
-									{-2, 0, 2},
-									{-1, 0, 1}	};
+	static double sobelKernelX[][] = { {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1} };
+	static double sobelKernelY[][];	// Not yet defined
 
 	/* NPU APPROXIMATION START */
-	public static double sobel(double[][] window){
-		// Note: See implementation in paper. 
-		double x, y, r; 
+	public static double sobel(double[][] window){ 
+		double x, y, r = 0; 
+		
 		x = ( window[0][0] + 2 * window[0][1] + window[0][2] ); 
-		x += ( window[2][0] + 2 * window[2][1] + window[2][2] );
+		x -= ( window[2][0] + 2 * window[2][1] + window[2][2] );
 		y = ( window[0][2] + 2 * window[1][2] + window[2][2] );
-		y += ( window[0][0] + 2 * window[1][1] + window[2][0]);
+		y -= ( window[0][0] + 2 * window[1][1] + window[2][0]);
 		r = Math.sqrt( (x*x) + (y*y) );
-		// if (r > 0.7071) r = 0.07070;
-		return 0.5;
+		if (r > 255.0) r = 0;
+		
+		/*
+		for (int y = 0; y < 3; y++){
+			for(int x = 0; x < 3; x++){
+				r+= sobelKernelX[x][y] * window[x][y];
+				r+= sobelKernelY[x]
+			}
+		}
+		*/
+		return r;
 	}
 	/* NPU APPROXIMATION END */
 
@@ -33,16 +41,29 @@ public class SobelFilter{
 		System.out.println("Red: "+red+" Green: "+green+" Blue: "+blue );
 	}
 
+	public static int getGreyScale(int clr){
+		int red = (clr & 0x00FF0000) >> 16; int green = (clr & 0x0000FF00)>>8; int blue = (clr & 0x000000FF);
+		if ( (red == blue) && (blue == green) ) return red;
+		else return -1;
+	}
+
+	public static int setGreyScaleValue(int clr){
+		return (clr) + (clr << 8) + (clr << 16);
+	}
+
 	public static double[][] buildWindow(int x, int y, BufferedImage srcImg){
 		double[][] retVal = new double[3][3];   
 		for ( int ypos = -1; ypos <= 1; ypos++ ){
 			for (int xpos = -1; xpos <= 1; xpos++ ){
 				int currX = xpos + x; int currY = ypos + y;
 				if ( (currX >= 0 && currX < width) && (currY >= 0 && currY < height) ){
-					retVal[xpos + 1][ypos + 1] = srcImg.getRGB(currX, currY);
+					int rgbRawValue = srcImg.getRGB(currX, currY);
+					int grayScaleValue = getGreyScale(rgbRawValue);
+					// retVal[xpos + 1][ypos + 1] = srcImg.getRGB(currX, currY);
+					retVal[xpos + 1][ypos + 1] = grayScaleValue;
 				}
 				else
-					retVal[xpos + 1][ypos + 1] = 0x00808080;
+					retVal[xpos + 1][ypos + 1] = 255;
 			}
 		}
 		return retVal; 
@@ -54,11 +75,20 @@ public class SobelFilter{
 		for (int y = 0; y < height; y++){
 			for (int x = 0; x < width; x++ ){
 				window = buildWindow(x, y, srcImg);
-				double newValue = sobel(window);
-				//retVal.setRGB(x, y, (int) Math.round(newValue) );
-				retVal.setRGB(x, y, 0x00808080);
-				System.out.println("X Loc: " + x + " Y Loc: " + y);
+				// double newValue = sobel(window);
+				double newValue = srcImg.getRGB(x,y);
+				double sobelValue = sobel(window);
+				int sobelRGBValue = setGreyScaleValue((int) sobelValue);
+				int grayScaleMag = getGreyScale( (int) newValue);
+				int greyscaleValue = setGreyScaleValue(grayScaleMag);
+				// retVal.setRGB(x, y, (int) Math.round(newValue) );
+				retVal.setRGB(x, y, sobelRGBValue);
+				System.out.println("X Loc: " + x + " Y Loc: " + y + " NewValue: " + (int) newValue);
+				printRGB( (int) newValue);
 				printRGB((int)window[1][1]);
+				System.out.println("Gray scale magnitude: " + grayScaleMag);
+				System.out.println("Gray scale value: " + greyscaleValue);
+				if(sobelValue < 255) System.out.println("Sobel value: " + sobelValue);
 			}
 		}
 		return retVal;
